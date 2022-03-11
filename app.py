69ff926c5ef47a94cc2e80ea29dc017b029a3b3b
Tashan Bedeau <me@tashan.co>
2022-03-11 13:35:51 +0000

    Added sqlite database

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pw_manager.db'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
