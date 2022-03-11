ef855a9c402d776130ed1141b38b8a14d145d0aa
Tashan Bedeau <me@tashan.co>
2022-03-11 12:32:48 +0000

    Added templates, index page and updated app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Initial setup to test if this is working. - Tashan</h1>'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
