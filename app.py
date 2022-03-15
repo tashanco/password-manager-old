14e7c55fc8404d2abd06213586d6515c84357d49
Tashan Bedeau <me@tashan.co>
2022-03-15 17:10:57 +0000

    Added password models, commenting and setup database

--- app.py
+++ app.py
@@ -5,6 +5,18 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pw_manager.db'
 db = SQLAlchemy(app)
 
+## Database Models
+
+class Password(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return '<Password %r>' % self.id
+
+## Routes
+
 @app.route('/')
 def index():
     return render_template('index.html')
